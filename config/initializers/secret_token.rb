dfa3ff7b7f671eb65c0a289e86cf60de3a5204ef
Gabe Morales <gabomorales@me.com>
2014-03-19 22:15:30 -0500

    Status App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Statusapp::Application.config.secret_key_base = '338a495c928dd31e5e759dd3cf95fb69a7d5f276947e08b12d9e4cf650c48a6846573c8c812f5ff3aefae9ab4646ca9516ac1b43e0276ec25e7e3346dadbc8f4'
